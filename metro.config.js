34141a8098b66d1c0e80cb8fca5288e435e69b2c
Randolf-717 <randolfsn77@gmail.com>
2025-09-24 23:47:18 -0400

    Completed Lab 2

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,16 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  codegenConfig: {
+    libraries: [
+      {
+        name: 'react-native-config',
+        type: 'modules',
+        jsSrcsDir: './node_modules/react-native-config/src',
+      },
+    ],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
