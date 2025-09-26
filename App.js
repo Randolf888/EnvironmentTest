bbf966d563173437aaec859345ee7378267396fb
Randolf-717 <randolfsn77@gmail.com>
2025-09-26 18:33:53 -0400

    LAB 2

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import {HomeScreen} from './src/screens';
 
 const App = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="#3498db" />
       <HomeScreen />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,7 +1,6 @@
 /**
  * @format
  */
-
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
